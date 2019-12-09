8a5539b3f7e570aaacaa99e185dae4883b6f7f73
Maykon Menezes <maykon.meneses@gmail.com>
2019-12-09 11:16:23 -0300

    User model and migrations generated

---
+++ test/controllers/demo_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class DemoControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get demo_index_url
+    assert_response :success
+  end
+
+  test "should get hello" do
+    get demo_hello_url
+    assert_response :success
+  end
+
+end
