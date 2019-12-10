37c171d40034a5a7bf1e103456db04a4548b7091
Maykon Menezes <maykon.meneses@gmail.com>
2019-12-10 10:11:14 -0300

    associations between models added

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,11 @@
 class Subject < ApplicationRecord
+
+  has_many :pages
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => true) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { where("created_at DESC") }
+  scope :visible, lambda { |query| where("name LIKE ?", "%#{query}%") }
+
 end
