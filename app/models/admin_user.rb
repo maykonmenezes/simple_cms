cd3b600e05a21cc1fca72f20a2e8413d6b07ec8d
Maykon Menezes <maykon.meneses@gmail.com>
2019-12-12 21:28:05 -0300

    User authentication added

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,8 @@
 class AdminUser < ApplicationRecord
 
+  has_secure_password
   has_and_belongs_to :pages
+
   has_many :section_edits
   has_many :sections, :through => :section_edits
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root "demo#index"
 
-  get "demo/index"
+  get 'admin', :to => 'access#menu'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  post 'access/logout'
 
   resources :subjects do
     member do
